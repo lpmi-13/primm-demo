18e691fa24af4422af4c0d12f598425f2f109d64
Adam Leskis <leskis@gmail.com>
2022-07-01 20:40:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const code1 = `
+def multiply(number):
+    return number * 2`;
+const code2 = `multiply(10)`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PRIMM Demo</h1>
+        <div className="display">
+          <code>
+            {code1}
+          </code>
+           <br></br>
+          <code>
+            {code2}
+          </code> 
+        </div>
       </header>
     </div>
   );
